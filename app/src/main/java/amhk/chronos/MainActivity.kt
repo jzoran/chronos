e92fec98ea4bdb4fd1dcc12313a45e291610f7d3
Mårten Kongstad <marten.kongstad@gmail.com>
2017-11-26 17:47:40 +0100

    Add navigation drawer
    Add a navigation drawer. When the user clicks a menu item, the main
    activity switches the currently displayed fragment.
    
    In this commit, the logic to switch fragments is triggered directly from
    the click listener. A future change will improve the application's
    design by introducing a lower layer Navigator class: when the user
    clicks a menu item, a request will be sent to the Navigator which then
    decides what fragment the main activity should switch to.
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- app/src/main/java/amhk/chronos/MainActivity.kt
+++ app/src/main/java/amhk/chronos/MainActivity.kt
@@ -1,20 +1,56 @@
 package amhk.chronos
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        val navigationView = findViewById<NavigationView>(R.id.main_navigation)
+        navigationView.setNavigationItemSelectedListener {
+            // FIXME: should just inform the lower layers of the user's intention
+
+            for (i in 0 until navigationView.menu.size()) {
+                val item = navigationView.menu.getItem(i)
+                item.isChecked = it.title == item.title
+            }
+
+            drawer.closeDrawer(navigationView, true)
+            when (it.title) {
+                "foo" -> switchToFragment(FooListFragment::class.java)
+                "bar"-> switchToFragment(BarFragment::class.java)
+                "bar 2" -> switchToFragment(BarFragment::class.java)
+                else -> false
+            }
+        }
+
         if (savedInstanceState == null) {
+            // FIXME: should call lower layers instead of making the switch explicitly
+            navigationView.menu.getItem(0).isChecked = true
             supportFragmentManager.transaction {
                 val fragment = FooListFragment()
                 add(R.id.main_container, fragment)
             }
         }
+    }
+
+    private fun <T> switchToFragment(x: Class<in T>): Boolean where T: Fragment {
+        supportFragmentManager.transaction {
+            val fragment = x.newInstance() as Fragment
+            replace(R.id.main_container, fragment)
+        }
+        return true
     }
 }
 
