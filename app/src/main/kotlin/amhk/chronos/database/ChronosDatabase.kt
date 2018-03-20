96ad083943f120c956acdf17705764c116e6c653
Zoran Jovanovic <zoran.jovanovic@sony.com>
2018-03-20 16:31:13 +0100

    Replace argument with 'it' in let block
    'let' takes 'this' as its argument so repeating 'value'
    is redundant when passed to formatter's parse method
    as non-null.
    
    Signed-off-by: Zoran Jovanovic <zoran.jovanovic@sony.com>
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- app/src/main/kotlin/amhk/chronos/database/ChronosDatabase.kt
+++ app/src/main/kotlin/amhk/chronos/database/ChronosDatabase.kt
@@ -39,7 +39,7 @@ internal object Converters {
     @JvmStatic
     fun toOffsetDateTime(value: String?): OffsetDateTime? {
         return value?.let {
-            return formatter.parse(value, OffsetDateTime::from)
+            return formatter.parse(it, OffsetDateTime::from)
         }
     }
 
